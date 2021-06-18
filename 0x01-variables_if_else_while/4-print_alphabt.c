a280bc40f8d9be3a4ad377e1424388f59152019a
Akwesi Kusi <therealkusi@gmail.com>
2021-06-18 12:32:42 -0700

    alphabets but not q or e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets but not e and q
+ *
+ * Description: using the main function
+ * this program prints alphabets but not e and q
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
