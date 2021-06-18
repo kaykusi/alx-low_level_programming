6b8aacfb950ecae520bf571939f57db214b3aa8c
Akwesi Kusi <therealkusi@gmail.com>
2021-06-18 13:03:25 -0700

    numbers with spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - printing numbers 
+ * Description: using the main function
+ * this program prints numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
