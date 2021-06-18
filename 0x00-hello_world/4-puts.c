814861c667baee3c69e919c988536f0a13211ab0
Akwesi Kusi <therealkusi@gmail.com>
2021-06-17 20:43:37 -0700

    first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
+
+/**
+ * main - print the string in the puts function
+ *
+ * Description: main function prints "Programming
+ * is like building a multilingual puzzle
+ * Return: 0
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
