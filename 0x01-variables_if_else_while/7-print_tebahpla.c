f2b98944fa07960969408ec34472c975d686f900
Akwesi Kusi <therealkusi@gmail.com>
2021-06-18 12:56:36 -0700

    alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in reverse
+ *
+ * Description: using the main function
+ * this program prints alphabets in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
