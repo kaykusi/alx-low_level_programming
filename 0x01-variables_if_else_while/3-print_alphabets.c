8e63454899e635ee64a7ad9c31675605b1255191
Akwesi Kusi <therealkusi@gmail.com>
2021-06-18 12:29:09 -0700

    alphabets in both caps

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lower case
+ *
+ * Description: using the main function
+ * this program prints alphabets
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
