fd933ba3aff4a49586f4c0ed368aa59adb19954b
Akwesi Kusi <therealkusi@gmail.com>
2021-06-18 10:20:45 -0700

    size of operands

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,12 +13,12 @@ int main(void)
 	char c;
 	int i;
 	long int li;
-	long long ll;
+	long long lli;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a char: %u byte(s)\n", sizeof(c));
+	printf("Size of an int: %u byte(s)\n", sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
-	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+	printf("Size of a long long int: %lli byte(s)\n", sizeof(lli));
+	printf("Size of a float: %f byte(s)\n", sizeof(f));
 	return (0);
