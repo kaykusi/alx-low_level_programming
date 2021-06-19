480dc78a07dfc550ed058b8827e94490717b4b68
Akwesi Kusi <therealkusi@gmail.com>
2021-06-19 05:43:53 -0700

    using write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <unistd.>
 
 /**
- * main - use printf function to print string
+ * main - use write function to print string
  *
  * Description: main function prints and that piece
  * of art is useful" - Dora Korpar, 2015-10-19
@@ -10,7 +11,7 @@
 
 int main(void)
 {
-	fscanf(stderr, "%s", "and that piece of art is useful\" -Dora Korpar,
-2015-10-19\n");
-	return (1);
+	write(2, "and that piece of art is useful\"
+- Dora Korpar, 2015-10-19\n", 59);
+	return (0);
 }
