25ee2dff9b96be4a0cdddd985db365d30ac34824
Akwesi Kusi <therealkusi@gmail.com>
2021-06-23 12:12:05 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,16 +10,16 @@ int print_last_digit(int d)
 {
 		int last_digit;
 
-		if (n > 0)
+		if (d > 0)
 		{
-			last_digit = n % 10;
-			putchar(last_digit + '0');
+			last_digit = d % 10;
+			_putchar(last_digit + '0');
 		}
 		else
 		{
-			n = -n;
-			last_digit = n % 10;
-			putchar(last_digit + '0');
+			d = -d;
+			last_digit = d % 10;
+			_putchar(last_digit + '0');
 		}
-		return (0);
+		return (d);
 }
