6bb09ca80e68c7f538c675f4052dead7c7a2d61b
Akwesi Kusi <therealkusi@gmail.com>
2021-06-23 05:37:19 -0700

    holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	char output[] = "Holberton";
 	int i = 0;
-	int length = sizeof(output) / sizeof(output[0]);
+	int length = sizeof(output) / sizeof(char);
 
 	while (i < length)
 	{
