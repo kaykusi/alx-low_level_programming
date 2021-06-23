10df55e5145637cc2f8b510684ad8f39368163c9
Akwesi Kusi <therealkusi@gmail.com>
2021-06-22 21:16:30 -0700

    print lower case alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,14 @@
 
 /**
  * main - check the code for ALX School students.
- *
+ * Description: prints lower case alphabets
  * Return: Always 0.
  */
 
 void print_alphabet(void)
 {
 	char l = 'a';
+	
 	while (l <= 'z')
 	{
 		_putchar(l);
