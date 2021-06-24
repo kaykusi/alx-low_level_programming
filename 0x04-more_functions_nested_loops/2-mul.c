a94fe23c3970e691e0e670bae320ed7b8720c06d
Akwesi Kusi <therealkusi@gmail.com>
2021-06-24 07:16:40 -0700

    multiply numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - function to multiply to numbers
+ * @a: integer to be checked
+ * @b: integer to be checked
+ * Return: product.
+ */
+
+int mul(int a, int b)
+{
+	int prod = a * b;
+
+	return (prod);
+}
