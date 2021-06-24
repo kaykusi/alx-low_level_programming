9cd3be286bd11af19c1c2719bd3098884894c49d
Akwesi Kusi <therealkusi@gmail.com>
2021-06-24 07:42:34 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints numbers from 0 - 9
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int digit = 48;
+
+	while (digit <= 57)
+	{
+		_putchar(digit);
+		_putchar('\n');
+		digit++;
+	}
+}
