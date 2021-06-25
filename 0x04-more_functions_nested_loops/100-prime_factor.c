12e91d821619de54a5da038cf571e37ea452b56c
Akwesi Kusi <therealkusi@gmail.com>
2021-06-25 05:21:40 -0700

    factorization

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - starting point for program execution
+ * Return: Always is 0
+ */
+
+int main(void)
+{
+	int i = 2, number;
+
+	printf("Enter number for prime factorization: ");
+	scanf("%d", number);
+
+	while (number != 1)
+	{
+		while (number % i == 0)
+		{
+			n /= i;
+			if (n == 1)
+			{
+				printf("%d", i);
+				printf("\n");
+			}
+		}
+		i++;
+	}
+}
