bf5dc843e5810b8c93c73253be22b7e2536d41b1
Akwesi Kusi <therealkusi@gmail.com>
2021-06-28 09:15:52 -0700

    swap two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps the value
+ * @a: integer to point to memory
+ * @b: integer to point to memory
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
