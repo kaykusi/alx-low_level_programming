7bd502bed1371bde9a988dd344bcf0f3d1b54ae7
Akwesi Kusi <therealkusi@gmail.com>
2021-06-29 07:04:43 -0700

    prints every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints each character
+ * @str: char pointer
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	while (str[count] != 0)
+	{
+		if (count % 2 == 0)
+			_putchar(str[count]);
+				 count++;
+	}
+	_putchar('\n');
+}
