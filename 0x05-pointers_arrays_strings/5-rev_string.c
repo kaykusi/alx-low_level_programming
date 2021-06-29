4c15737ac831bb1c51539d06eca03060d135eceb
Akwesi Kusi <therealkusi@gmail.com>
2021-06-29 06:06:16 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,21 +8,22 @@
 
 void rev_string(char *s)
 {
-	int rev, i, count = 0;
+	int rev, count = 0, i = 0;
 	char ch;
 
-	while (s[count] != '\0')
-	{
-		count++;
-	}
-	rev = count - 1;
 
-	while (rev > i)
-	{
-		ch = s[i];
-		s[i] = s[rev];
-		s[rev] = ch;
-		rev--;
-		i++;
-	}
+		while (s[count] != '\0')
+		{
+			count++;
+		}
+		rev = count - 1;
+
+		while (rev > i)
+		{
+			ch = s[i];
+			s[i] = s[rev];
+			s[rev] = ch;
+			rev--;
+			i++;
+		}
 }
