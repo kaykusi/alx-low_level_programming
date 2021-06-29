6104da5978bef0f851e033b998292a35cdacfb45
Akwesi Kusi <therealkusi@gmail.com>
2021-06-29 05:57:14 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: char pointer
+ * Return: Void
+ */
+
+void rev_string(char *s)
+{
+	int rev, i, count = 0;
+	char ch;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	rev = count - 1;
+
+	while (rev > i)
+	{
+		ch[i] = s[i];
+		s[i] = s[rev];
+		s[rev] = ch;
+		rev--;
+		i++;
+	}
+}
