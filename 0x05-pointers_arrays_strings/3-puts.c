3a8af0414c4dce48f91088c9c993befcc026607c
Akwesi Kusi <therealkusi@gmail.com>
2021-06-29 01:44:19 -0700

    print to screen

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _puts - function to print a string
+ * @str: pointer to memory
+ * Return: Void
+ */
+
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (str[count] != '\0')
+	{
+		_putchar(str[count]);
+			 count++;
+	}
+}
