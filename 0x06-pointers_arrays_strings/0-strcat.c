dfe6a0c0e17c46a48331f9ea1ac9f51bdc4ef922
Akwesi Kusi <therealkusi@gmail.com>
2021-07-01 08:57:46 -0700

    concat two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strcat - pointer function to concat
+ * @dest: char pointer
+ * @src: char pointer
+ * Return: char value
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int count = 0, c;
+
+	while (dest[count] != '\0')
+	{
+		count++;
+	}
+	while (src[c] != '\0')
+	{
+		dest[count] = src[c];
+		c++;
+		count++;
+	}
+	return (dest);
+}
