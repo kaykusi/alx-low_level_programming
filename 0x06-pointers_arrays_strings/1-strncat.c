05d36eaabbf28c735a3d3130cb74e19ff735b2f6
Akwesi Kusi <therealkusi@gmail.com>
2021-07-01 09:22:59 -0700

    concat two strings with max characters

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strncat - main entry
+ * @dest: char pointer
+ * @src: char pointer
+ * @n: integer value
+ * Descrption: concatenate two strings with max numbers
+ * Return: resulting string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int count = 0, c = 0;
+
+	while (dest[count] != '\0')
+	{
+		count++;
+	}
+	while (src[c] != '\0' && c < n)
+	{
+		dest[count] = src[c];
+		c++;
+		count++;
+	}
+	return (dest);
