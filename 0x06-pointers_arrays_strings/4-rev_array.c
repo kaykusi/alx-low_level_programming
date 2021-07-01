b367487058ab6a430b4732831369033451541966
Akwesi Kusi <therealkusi@gmail.com>
2021-07-01 13:11:50 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverse array
+ * @a: integer pointer
+ * @n: integer pointer
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0, j;
+
+	while (i < n--)
+	{
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+		n--;
+		i++;
+	}
+}
