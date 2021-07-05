524cd260b36700cabdfe5274e9aae10d58972a81
Akwesi Kusi <therealkusi@gmail.com>
2021-07-05 12:10:22 -0700

    search through a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strchr - function to search a string
+ * @s: char pointer
+ * @c: char value
+ * Return: char pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+	else
+		return (NULL);
+}
