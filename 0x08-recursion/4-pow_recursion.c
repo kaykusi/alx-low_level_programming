21384ab95a8e416fd3be36ad6cb74121b628082d
Akwesi Kusi <therealkusi@gmail.com>
2021-07-07 12:56:14 -0700

    exponent

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - function to find exponent of a number
+ * @x: integer value for base
+ * @y: integer value for power
+ * Return: integer value of result
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
