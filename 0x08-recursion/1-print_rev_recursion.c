2c6818d14c9f6667c07d6718c5600f9908b28687
Akwesi Kusi <therealkusi@gmail.com>
2021-07-07 08:28:30 -0700

    print reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - function to print reverse string
+ * @s: char pointer
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+		_print_rev_recursion(s + 1);
+	putchar(s[i]);
+}
