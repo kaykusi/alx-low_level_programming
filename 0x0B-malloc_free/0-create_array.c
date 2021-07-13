3ad9c387a8718e7c3ebd98ec82ce023398e9f82f
Akwesi Kusi <therealkusi@gmail.com>
2021-07-13 11:18:10 -0700

    array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * create_array - function for array pointer
+ * @size: integer to determine size of array
+ * @c: char for array values
+ * Return: char value
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *m;
+
+	m = malloc(sizeof(c) * size);
+
+	while (i < size)
+	{
+		if (m == NULL)
+		{
+			return (NULL);
+		}
+		m[i] = c;
+		i++;
+	}
+	return (m);
+}
