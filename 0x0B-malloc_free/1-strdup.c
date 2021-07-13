1b659df422c779deb1151e4ecc3c1f466a366b9c
Akwesi Kusi <therealkusi@gmail.com>
2021-07-13 11:54:41 -0700

    copy of string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - function returns newly allocted memory
+ * which contains a copy
+ * @str: char pointer
+ * Return: char value
+ */
+
+char *_strdup(char *str)
+{
+	char *m;
+	int i = 0 len = 0;
+
+	if (str[i] == NULL)
+		return (NULL);
+
+	m = malloc(sizeof(str));
+
+	if (m == NULL)
+		return (NULL);
+	while (str[i])
+	{
+		len++;
+	}
+	int i = 0;
+
+	while (str[i])
+	{
+		m[i] = str[i];
+		i++;
+	}
+	return (m);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -2,4 +2,5 @@
 #define _HOLBERTON_H_
 
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 #endif /* _HOLBERTON_H_ */
