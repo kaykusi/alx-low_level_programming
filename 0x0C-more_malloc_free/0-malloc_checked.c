7f9472ef7ae7590e53fe0731b0eba47a5d453f03
Akwesi Kusi <therealkusi@gmail.com>
2021-07-15 18:30:51 -0700

    allocate memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function that aloocated memory using malloc
+ * @b: unsigned integer value
+ * Return: pointer to memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		return (98);
+	return (ptr);
+}
