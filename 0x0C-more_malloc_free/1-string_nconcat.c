54d1320382914dcdd9a37387220d2add87049b89
Akwesi Kusi <therealkusi@gmail.com>
2021-07-15 19:21:39 -0700

    concat string

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - function that concats two strings
+ * @s1: char pointer
+ * @s2:  char pointer
+ * @n: bytes of string
+ * Return: Null if fail, pointer to memory
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *strcat;
+	unsigned int len = n, i = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[i])
+	{
+		len++;
+		i++;
+	}
+	strcat = malloc(sizeof(*strcat) * (len + 1));
+
+	if (strcat != NULL)
+	{
+		len = 0;
+
+		for (i = 0; s1[i]; len++, i++)
+			strcat[len] = s1[i];
+
+		for (i = 0; s2[i] && i < n; len++, i++)
+			strcat[len] = s2[i];
+
+		strcat[len] = '\0';
+
+		return (strcat);
+	}
+	else
+		return (NULL);
+}
