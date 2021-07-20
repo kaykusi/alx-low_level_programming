fb9737d70a1280eef696f00fc7e4bea1bc357e17
Akwesi Kusi <therealkusi@gmail.com>
2021-07-20 06:43:02 -0700

    initializing

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+
+/**
+ * init_dog - function to initialize struct
+ * @d: the struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner's name
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
