c8ec0596f63210a9218e6036f06b7d5d49cb2d49
Akwesi Kusi <therealkusi@gmail.com>
2021-07-20 06:33:49 -0700

    structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - main structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif /* _DOG_H_ */
