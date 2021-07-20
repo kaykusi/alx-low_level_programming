e735e6dfd5eb2734497304612249945656c71c25
Akwesi Kusi <therealkusi@gmail.com>
2021-07-20 07:13:39 -0700

    print names

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,6 +14,6 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
 		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+		printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
 	}
 }
