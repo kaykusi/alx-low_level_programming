3a7f441a458a59dd412339f4b2c7fae469e118f1
Akwesi Kusi <therealkusi@gmail.com>
2021-07-22 05:20:12 -0700

    simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,7 +10,7 @@
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
-	char *arr;
+	char *op;
 
 	if (argc != 4)
 	{
@@ -19,23 +19,23 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	}
 
 	num1 = atoi(argv[1]);
-	arr = argv[2];
+	op = argv[2];
 	num2 = atoi(argv[3]);
 
-	if (get_op_func(arr) == NULL || arr[1] != '\0')
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*arr == '/' && num2 == 0) ||
-	    (*arr == '%' && num2 == 0))
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(arr)(num1, num2));
+	printf("%d\n", get_op_func(op)(num1, num2));
 
 	return (0);
 }
