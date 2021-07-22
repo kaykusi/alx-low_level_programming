2208f31fe63de6dfd621b512165292b61f086766
Akwesi Kusi <therealkusi@gmail.com>
2021-07-22 01:50:54 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that prints name
+ * @name: char pointer
+ * @f: function pointer
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
