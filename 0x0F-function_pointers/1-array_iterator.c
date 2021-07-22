54e6ef1ef36f60e25bb5ad46d4277c63076cf79c
Akwesi Kusi <therealkusi@gmail.com>
2021-07-22 03:37:24 -0700

    array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - A function that
+ * executes a function on each element of an array
+ * @array: integer pointer
+ * @size: size of the array
+ * @action: pointer to the function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t a;
+
+	if (array && action)
+	{
+		for (a = 0; a < size; a++)
+			action(array[a]);
+	}
+}
