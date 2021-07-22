cb38f4402689de1729c67644148bea0bdc34f1c1
Akwesi Kusi <therealkusi@gmail.com>
2021-07-22 02:43:40 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+
 /**
  * print_name - function that prints name
  * @name: char pointer
@@ -7,5 +8,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name && f)
 	(*f)(name);
 }
