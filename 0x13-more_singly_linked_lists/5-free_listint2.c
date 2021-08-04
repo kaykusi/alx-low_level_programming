40e7b876d60e43a596b8f7e6394b30e8cc6ef7fc
Akwesi Kusi <therealkusi@gmail.com>
2021-08-04 03:41:13 -0700

    free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 -free list.
+ * @head: pointer to a pointer
+ * Return: void.
+ **/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+	temp = *head;
+
+	while (temp)
+	{
+		temp = temp->next;
+		free(*head);
+		*head =temp;
+	}
+	*head = NULL;
+}
