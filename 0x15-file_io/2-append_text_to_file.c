5eed3e856f1b3552bfe4bc704699950ef09ad5e6
Akwesi Kusi <therealkusi@gmail.com>
2021-08-12 00:25:13 -0700

    append text

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - append text
+ * @filename: the name of the file
+ * @text_content: is a NULL terminated string to append to the file
+ * Return:1succes -1failure
+ **/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int wr = 0, len;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_RDWR | O_APPEND, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
+		wr = write(fd, text_content, len);
+
+		if (wr == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
