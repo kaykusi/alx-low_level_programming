997b8a692b53dd47dbd49c29000c71baf475622d
Akwesi Kusi <therealkusi@gmail.com>
2021-08-12 00:22:36 -0700

    creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the name of the file to create
+ * @text_content: is a NULL terminated string to write to the file
+ * Return:1succes -1failure(file cant be created,cant be written, write "fails)
+ **/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int wr = 0, len;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
+		wr = write(fd, text_content, len);
+
+		if (wr == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
