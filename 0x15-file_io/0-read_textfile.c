dc23c84f15d17237129e403fec18c41b08aef8ce
Akwesi Kusi <therealkusi@gmail.com>
2021-08-12 00:32:46 -0700

    reads text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,31 +9,28 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t r, w;
+	ssize_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDWR);
-	if (fd == -1)
-		return (0);
-	buffer = malloc(letters);
+
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-	{
-		close(fd);
 		return (0);
-	}
-	r = read(fd, buffer, letters);
-	close(fd);
-	if (r == -1)
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
 	}
-	w = write(STDOUT_FILENO, buffer, r);
+
 	free(buffer);
-	if (r != w)
-		return (0);
+	close(o);
+
 	return (w);
 }
