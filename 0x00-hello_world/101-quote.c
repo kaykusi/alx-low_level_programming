df67738c281589f99eead47f87394146dcc47d78
Akwesi Kusi <therealkusi@gmail.com>
2023-08-10 16:39:09 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Prints exactly and that piece of art is useful -
@@ -7,7 +8,7 @@
  *
  * Return: Always 1.
  */
-int main()
+int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 
