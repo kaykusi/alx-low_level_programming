3403233f18ef041d140e84143d6f0df85a857384
Akwesi Kusi <therealkusi@gmail.com>
2023-08-10 16:36:41 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints exactly and that piece of art is useful -
+ * 	  Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ *                
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+
+	return (1);
+}
