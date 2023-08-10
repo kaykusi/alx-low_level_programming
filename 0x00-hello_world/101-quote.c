5d4f2ba71062dfbd4e429df280771cdfa5871fdd
Akwesi Kusi <therealkusi@gmail.com>
2023-08-10 16:37:42 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  *
  * Return: Always 1.
  */
-int main(void)
+int main()
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 
