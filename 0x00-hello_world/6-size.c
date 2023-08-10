6576abea81c09d9b9dc860e356776bf248c033a0
Akwesi Kusi <therealkusi@gmail.com>
2023-08-10 16:24:51 -0700

    all sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-	puts("Size of a char: " + sizeof(char) - 1);
-	puts("Size of an int: " + sizeof(int) - 1);
-	puts("Size of a long int: " + sizeof(long int) - 1);
-	puts("Size of a long long int: " + sizeof(long long int) - 1);
-	puts("Size of a float: " + sizeof(float) - 1);
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
 }
