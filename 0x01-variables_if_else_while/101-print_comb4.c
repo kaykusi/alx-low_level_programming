7446b6cdfe9b335a13043f4b49651e8dc6d7a36e
Akwesi Kusi <therealkusi@gmail.com>
2023-08-13 18:26:24 -0700

    pus

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,36 +1,30 @@
 #include <stdio.h>
-
 /**
- *main - print all combinations of three different digits
- *Return: Always 0 (Success)
+ * main - Prints all possible combinations of three different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	int first_digit;
-	int second_digit;
-	int third_digit;
+	int digit1, digit2, digit3;
 
-	for (third_digit = '0'; third_digit <= '9'; third_digit++) /*hundreds place*/
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-		for (second_digit = (third_digit + 1); second_digit <= '9'; second_digit++)
-			/*tens place*/
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-			for (first_digit = (first_digit + 1); first_digit <= '9'; first_digit++)
-				/*ones place*/
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
-				putchar(third_digit);
-				putchar(second_digit);
-				putchar(first_digit);
-				if (third_digit != '7' || second_digit != '8' || first_digit != '9')
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
