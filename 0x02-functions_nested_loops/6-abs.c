228c1d10157bc89b011387eae483685f28048c71
Akwesi Kusi <therealkusi@gmail.com>
2023-08-16 15:51:47 -0700

    last

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,27 +1,20 @@
 #include "main.h"
 
 /**
- * print_sign - prints whether a number is positive, negative or zero
- * @n: number to be checked
+ * _abs - prints absolute value of a number
  *
- * Return: 1 if positive, 0 if zero, -1 if negative
+ * @n: integer to be computed
+ * Return: integer value
  */
 
-int print_sign(int n)
+int _abs(int n)
 {
-	if (n > 0)
+	if (n >= 0)
 	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		return (-1);
+		return (n);
 	}
 	else
 	{
-		_putchar('0');
-		return (0);
+		return (-n);
 	}
 }
